c55f6cea50c0701ede31860a47d38581f2ca3a63
Sihkdragon <dithor37@gmail.com>
2022-03-02 03:49:56 +0700

    add home and tw function

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,7 +14,7 @@ class BlogController extends Controller
      */
     public function index()
     {
-        //
+        return view('Blog.blog');
     }
 
     /**
@@ -46,7 +46,7 @@ class BlogController extends Controller
      */
     public function show(Blog $blog)
     {
-        //
+        return Blog::all();
     }
 
     /**
